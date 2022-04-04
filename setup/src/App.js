c5d9cf4395953acc01e990db475cada4b4519045
Muhammad Asif <asif88383@gmail.com>
2022-04-04 00:11:46 -0700

    added list data

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data);
   return (
   <main>
-    <h2>reminder project setup</h2>
+    <section className='container'>
+      <h3>{people.length} Birthdays Today!</h3>
+      <List people={people}/>
+      <button onClick={() => console.log
+        ('you clicked me')}>Clear all</button>
+    </section>
   </main>
   );
 }
