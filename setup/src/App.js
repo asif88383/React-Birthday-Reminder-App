65374e905be869ab1599421880b7e1d4a0c915c1
Muhammad Asif <asif88383@gmail.com>
2022-04-03 23:53:51 -0700

    first commit

---
+++ setup/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return (
+  <main>
+    <h2>reminder project setup</h2>
+  </main>
+  );
+}
+
+export default App;
